13461a6d5185ac442190dadea3c0bbae0a7b2282
Mia <abresnahan7@SamComputer.lewisu.local>
2023-11-01 10:31:17 -0500

    update files.

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,8 +1,11 @@
 import os 
 
 print("Add Commit Push") 
-# print("\ngit status")
-# os.system("git status") 
-# os.system("git add -A")
-os.system("git commit -m 'Update files.'")
-# os.system("git push")
+print("\ngit status")
+os.system("git status") 
+print("\ngit add -A")
+os.system("git add -A")
+print("\ngit commit -m 'update files.'")
+os.system("git commit -m 'update files.'")
+print("\ngit push")
+os.system("git push")
